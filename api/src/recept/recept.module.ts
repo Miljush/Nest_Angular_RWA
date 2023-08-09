9854011ea6c50dc286676ea353d10c5e772e9808
Miljush <milosjovanovic2001@gmail.com>
2023-08-09 02:39:22 +0200

    dodate metode i funkcionalnosti

--- api/src/recept/recept.module.ts
+++ api/src/recept/recept.module.ts
@@ -3,10 +3,12 @@ import { ReceptifeedService } from './services/recept.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReceptPostEntity } from './models/recept.entity';
 import { ReceptifeedController } from './controllers/recept.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports:[
-    TypeOrmModule.forFeature([ReceptPostEntity])
+    UserModule,TypeOrmModule.forFeature([ReceptPostEntity]),
+    
   ],
   providers: [ReceptifeedService],
   controllers:[ReceptifeedController]
--- api/src/recept/services/recept.service.ts
+++ api/src/recept/services/recept.service.ts
@@ -1,25 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { ReceptPostEntity } from '../models/recept.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Recept } from '../models/recept.interface';
+import { CreateReceptDto } from '../dto/createreceptdto';
+import { UserService } from 'src/user/services/user.service';
+import { STATUS_CODES } from 'http';
+
 
 @Injectable()
 export class ReceptifeedService {
     constructor(
         @InjectRepository(ReceptPostEntity)
-        private readonly receptiPostRepository:Repository<ReceptPostEntity>
+        private readonly receptiPostRepository:Repository<ReceptPostEntity>,
+        @Inject(UserService)
+        private readonly userService: UserService,
+        
     ){}
 
-    createRecept(recept: Recept){
+    async createRecept(createReceptDto: CreateReceptDto): Promise<ReceptPostEntity> {
+        
+        const userizbaze=await this.userService.vratiUseraZaId(createReceptDto.id)
+        const recept = new ReceptPostEntity();
+        recept.user=userizbaze;
+        recept.ime=createReceptDto.ime;
+        recept.opis=createReceptDto.opis;
+        recept.sastojci=createReceptDto.sastojci;
+        recept.slike=createReceptDto.slike;
+    
         return this.receptiPostRepository.save(recept);
-    }
+      }
 
     vratiSveRecepte(){
-        return this.receptiPostRepository.find();
+        return this.receptiPostRepository
+      .createQueryBuilder('recept')
+      .leftJoinAndSelect('recept.user', 'user')
+      .select(['recept', 'user.id'])
+      .getMany();
     }
-    azurirajRecept(id:number,recept:Recept){
+    azurirajRecept(id:number,recept:ReceptPostEntity){
         return this.receptiPostRepository.update(id,recept)
+    }
+    async lajkujRecept(idrecepta:number,idusera:number){
+        const rec=await this.receptiPostRepository.findOneById(idrecepta)
+        if(rec!=null){
+        return this.userService.lajkuj(idrecepta,idusera)
+        }
+        else{
+            throw new BadRequestException("Recept nije prondadjen!");
+        }
+        
+
     }
     izbrisiRecept(id:number){
         return this.receptiPostRepository.delete(id);
